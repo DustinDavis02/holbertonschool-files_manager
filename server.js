6abfe53c078bc020b011b7036d9a1ab75ad4f2f6
Dustin Davis <davisdustin328@gmail.com>
2023-11-21 15:02:46 -0600

    Parsing Middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import routes from './routes/index';
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.use('/', routes);
