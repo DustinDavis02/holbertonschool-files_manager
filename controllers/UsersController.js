5b4206a7dc24c6bcff7d67c70a45368131fa82c6
Dustin Davis <davisdustin328@gmail.com>
2023-11-21 15:20:37 -0600

    User controller update

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -29,6 +29,21 @@ class UsersController {
 
     return res.status(201).json({ id: newUser.insertedId, email });
   }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
 }
 
 export default UsersController;
